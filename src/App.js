30e1acd5ade36e4f3d4994aa8894a98cdec1f7f6
Kamal <kkjoshi.kk@gmail.com>
2023-05-08 21:41:22 +0530

    minor fx

--- src/App.js
+++ src/App.js
@@ -10,16 +10,11 @@ const App = ()=> {
   const pageSize = 5;
   const apiKey = process.env.REACT_APP_NEWS_API
   const [progress, setProgress] = useState(0)
- 
-    return (
-      <div>
-        <Router>
-        <NavBar/> 
-        <LoadingBar
-        height={3}
-        color='#f11946'
-        progress={progress} 
-      />
+  return (
+    <div>
+      <Router>
+        <NavBar/>
+        <LoadingBar height={3} color='#f11946'progress={progress} />
         <Switch>
           <Route exact path="/"><News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/></Route> 
           <Route exact path="/business"><News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business"/></Route> 
@@ -30,10 +25,9 @@ const App = ()=> {
           <Route exact path="/sports"><News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports"/></Route> 
           <Route exact path="/technology"><News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology"/></Route> 
         </Switch>
-        </Router>
-      </div>
-    )
- 
+      </Router>
+    </div>
+  )
 }
 
 export default App;
